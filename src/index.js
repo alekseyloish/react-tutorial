e8ce2e92f67029324e315252f993fe34af25bc54
alekseyloish <aleksey.loish@gmail.com>
2020-03-19 02:08:58 +0200

    lesson 3 - state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Lesson from './02_components/Lesson.jsx';
+// import Lesson from './02_components/Lesson.jsx';
+
+import CounterButton from './03_state/Lesson.jsx';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Lesson />, document.getElementById('root'));
+ReactDOM.render(<CounterButton />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
